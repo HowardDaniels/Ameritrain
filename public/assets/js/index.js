23f0dab95e8a4ef0c05fe80a7b9346b29e871856
Howard Daniels <hdaniels13tat@gmail.com>
2022-07-17 18:52:37 -0400

    added app.get and db folder and js file

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var mysql = require("mysql");
 var app = express();
-/*
+
+app.get('/', function(req, res){
+    res.send('Hey there!');
+});
+
 app.listen(3000, function(){
     console.log("App listening on port 3000.");
-}); */
+});
